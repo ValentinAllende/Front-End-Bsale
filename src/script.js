dcf44c0550e7ca24b233829a1d5507a7c8a8f6c5
ValentinAllende <valentin_allende@hotmail.com>
2022-11-15 08:24:25 -0300

    actualizado

--- src/script.js
+++ src/script.js
@@ -2,7 +2,38 @@ const API_URL = "https://back-end-bsal.herokuapp.com";
 const useCardTemplate = document.querySelector("[data-user-template]");
 const userCardContainer = document.querySelector("[data-cards-container]");
 const searchInput = document.querySelector("[data-search]");
-
+const selectOption = document.getElementById("select");
+//filtrado por Categoria
+//https://back-end-bsal.herokuapp.com/products/category/1
+selectOption.addEventListener("change", (e) => {
+  const valor = e.target.value;
+  console.log(valor);
+  if (valor !== "") {
+    document.getElementById("card-container").innerHTML = "";
+    const selceted = fetch(`${API_URL}/products/category/${valor}`, {
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "GET",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        data.map((x) => {
+          const card = useCardTemplate.content.cloneNode(true).children[0];
+          const name = card.querySelector("[data-name]");
+          const price = card.querySelector("[data-price]");
+          const discount = card.querySelector("[data-discount]");
+          const image = card.querySelector("[data-image]");
+          name.textContent = x.name;
+          price.textContent = x.price;
+          discount.textContent = x.discount;
+          image.textContent = x.url_image;
+          userCardContainer.append(card);
+        });
+      });
+  }
+});
 //Filtrado por Nombre
 searchInput.addEventListener("keypress", (e) => {
   if (e.key === "Enter") {
@@ -59,26 +90,3 @@ const response = fetch(`${API_URL}/products`, {
       userCardContainer.append(card);
     });
   });
-
-//buscador desde el back
-// if (!"") {
-//   const filter = fetch(`${API_URL}/products?search=${value}`, {
-//     mode: "cors",
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//     method: "GET",
-//   })
-//     .then((res) => res.json())
-//     .then((data) => console.log("filtrado: " + data));
-// }
-// document.addEventListener("DomContentLoaded", fetchingProducts, false);
-// const search = () => {
-//   //INPUT SEARCH BAR
-//   const input = document.getElementById("search-navbar");
-
-//   input.addEventListener("input", (e) => {
-//       const value = e.target.value;
-//       console.log('input: ' + value);
-//   })
-// };
